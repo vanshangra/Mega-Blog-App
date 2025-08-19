928b16d97324e14771614cfe35986a6bf4912150
Vansh Angra <146015188+vanshangra@users.noreply.github.com>
2025-08-19 18:02:15 +0000

    changes

--- Megablog/src/components/Header/Header.jsx
+++ Megablog/src/components/Header/Header.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Container, Logo, LogoutBtn } from '../Index'
+import { Container, Logo, LogoutBtn } from '..'
 import { Link, useNavigate } from 'react-router-dom'
 import { useSelector } from 'react-redux'
 
@@ -40,20 +40,20 @@ function Header() {
         <nav className='flex'>
           <div className='mr-4'>
             <Link to='/'>
-              <Logo width='70px'  />
+              <Logo width='70px' />
 
             </Link>
           </div>
           <ul className='flex ml-auto'>
-            {navItems.map((item) => 
-            item.active ? (
-              <li key={item.name}>
-                <button
-                onClick={() => navigate(item.slug)}
-                className='inline-bock px-6 py-2 duration-200 hover:bg-blue-100 rounded-full'
-                >{item.name}</button>
-              </li>
-            ) : null
+            {navItems.map((item) =>
+              item.active ? (
+                <li key={item.name}>
+                  <button
+                    onClick={() => navigate(item.slug)}
+                    className='inline-bock px-6 py-2 duration-200 hover:bg-blue-100 rounded-full'
+                  >{item.name}</button>
+                </li>
+              ) : null
             )}
             {authStatus && (
               <li>
---
+++ Megablog/src/components/Signup.jsx
@@ -0,0 +1,10 @@
+import React, {useState} from 'react'
+
+
+function Signup() {
+  return (
+    <div>Signup</div>
+  )
+}
+
+export default Signup
